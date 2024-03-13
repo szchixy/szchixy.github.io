638d387cc8efdb23aa4aac811c4c0cca70e8ac8f
Geogi Chi <szchixy@gmail.com>
2024-03-13 16:02:12 +0800

    add .,gitattributes, fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,10 @@ cd docs/.vitepress/dist
 
 git init -b gh-pages
 git remote add origin https://github.com/szchixy/szchixy.github.io.git
-git config --local user.name szchixy
-git config --local user.email szchixy@outlook.com
+git config --local user.name 'Geogi Chi'
+git config --local user.email szchixy@gmail.com
 
 git add -A
 git commit -m "auto deploy"
 
-git push origin gh-pages
+git push origin gh-pages --force
